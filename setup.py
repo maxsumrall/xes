f431481c36eb6e138cb37f830264468465df3eea
Jonathan Sumrall <jmsumrall@gmail.com>
2014-08-15 17:28:27 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='xes',
-    version='1.11',
+    version='1.2',
     packages=['xes'],
     url='http://pypi.python.org/pypi/xes/',
     license='Apache License 2.0',
     author='Jonathan Sumrall',
-    author_email='jmsumrall@gmail.com',
+    author_email='j.m.sumrall@student.tue.nl',
     description='A simple tool for generating XES files for Process Mining',
     long_description=open('README.txt').read()
 )
